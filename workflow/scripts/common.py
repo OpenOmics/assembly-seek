664d7365bbfe383ab4d8f4f6470c90687f044dd7
skchronicles <kuhnsa3@gmail.com>
2024-03-06 18:37:36 -0500

    Fixing input BAM dry-run errors

--- workflow/scripts/common.py
+++ workflow/scripts/common.py
@@ -1,6 +1,7 @@
 # Common helper functions shared across the entire workflow
 import sys, os
 
+
 def identify_samples_as_bam_fastq(samplelist, condition):
     """
     From sample names determines which of the input files have bam or fastq extensions.
@@ -45,14 +46,6 @@ def provided(samplelist, condition):
     return samplelist
 
 
-def check_readaccess(filename):
-    """Checks permissions to see if user can read a file
-    :param filename <str>: Name of file to check
-    """
-    if not os.access(filename,os.R_OK):
-        sys.exit("File or Folder: {} exists, but user cannot read from file due to permissions!".format(filename))
-
-
 def ignore(samplelist, condition):
     """
     Determines if optional rules should run. If an empty list is provided to rule all,
@@ -69,95 +62,6 @@ def ignore(samplelist, condition):
         samplelist = []
 
     return samplelist
-
-
-def s3_configured(uri):
-    """
-    Determines if user can access s3 object using their credentials saved in
-    "~/.aws/credentials" or "~/.boto". This handles an edge case where a user
-    has aws configure on the target system but the AWS Access Key Id provided in
-    their config does not match s3 access policy. This usually occurs when a user
-    has setup aws but it is setup for another AWS account or one that does not
-    have an IAM role for our S3 bucket.
-    :param uri <str>: URI/URL to object in S3 bucket
-    :return accessible <boolean>:
-        True if user can access S3 object, False if user cannot access the object (403)
-    """
-    import boto3
-    import botocore
-    import re
-
-    # Get bucket and key from s3 uri
-    parsed = re.match(r's3:\/\/(.+?)\/(.+)', uri)
-    bucket, key = parsed.groups()
-    accessible = True
-
-    try:
-        # Try to access object in s3 bucket
-        boto3.resource("s3").Object(bucket, key).load()
-    except botocore.exceptions.ClientError as e:
-        # User cannot access object in S3 bucket with the credentials
-        # stored in "~/.aws/credentials" or "~/.boto".
-        if e.response["Error"]["Code"] == "403":
-            accessible = False
-
-    return accessible
-
-
-def abstract_location(file_address, *args, **kwargs):
-    """
-    Determines if a provided file or list of file(s) resides in a remote location.
-    If file(s) are determined to reside in remote store, like a S3 or Google Cloud
-    Storage, Snakemake's remote wrapper is used to defined remote files.
-    This can be extended further to support more file types listed here.
-    https://snakemake.readthedocs.io/en/stable/snakefiles/remote_files.html
-    Supported remotes file options include: s3, gs, and sftp
-    Input: File path <str> or a list or file paths list[<str>]
-    Output: List of files or remote objects
-    """
-
-    # Check if user provided any input
-    if not file_address or file_address is None:
-        raise IOError("Failed to provide any input files! Input(s) are required to resolve required files.".format(file_address))
-
-    # If given file path to one file, convert it a list[<str>]
-    file_list = [file_address] if isinstance(file_address, str) else file_address
-
-    # Loop through list of provided files, and if a remote storage option
-    # is given, convert its index to a remote file object.
-    for i, uri in enumerate(file_list):
-        if uri.lower().startswith('s3://'):
-            # Remote option for S3 storage
-            import snakemake.remote.S3
-            import botocore.session
-
-            if botocore.session.get_session().get_credentials() and s3_configured(uri):
-                # AWS cli or boto has been configured on target system
-                # See ~/.aws/credentials or ~/.boto
-                # https://boto.readthedocs.io/en/latest/boto_config_tut.html
-                remote_provider = snakemake.remote.S3.RemoteProvider()
-            else:
-                # If botocore cannot find credentials, try connecting unsigned.
-                # This will work for anonymous S3 resources if the resources in the
-                # s3 bucket are configured correctly.
-                # If a file in provieded as input to a Snakemake rule, only read
-                # access is needed to access the remote S3 object.
-                remote_provider = snakemake.remote.S3.RemoteProvider(config=botocore.client.Config(signature_version=botocore.UNSIGNED))
-            file_list[i] = remote_provider.remote(uri, *args, **kwargs)
-
-        elif uri.lower().startswith('gs://'):
-            # Remote option for Google Cloud Storage
-            import snakemake.remote.GS
-            remote_provider = snakemake.remote.GS.RemoteProvider()
-            file_list[i] = remote_provider.remote(uri, *args, **kwargs)
-
-        elif uri.lower().startswith('sftp://'):
-            # Remote option for SFTP transfers
-            import snakemake.remote.SFTP
-            remote_provider = snakemake.remote.SFTP.RemoteProvider()
-            file_list[i] = remote_provider.remote(uri, *args, **kwargs)
-
-    return file_list
 
 
 def references(config, reflist):
